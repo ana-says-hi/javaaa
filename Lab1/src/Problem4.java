99edbec37ffe4ea23b7d327ea070916f5f1bd248
ana-says-hi <120421396+ana-says-hi@users.noreply.github.com>
2023-10-13 17:05:44 +0300

    Update Problem4.java

--- Lab1/src/Problem4.java
+++ Lab1/src/Problem4.java
@@ -1,2 +1,25 @@
 public class Problem4 {
+    /**
+     * Markus will ein USB Laufwerk und eine Tastatur kaufen. Der Elektronik-Shop hat verschiedene USB Laufwerke
+     * und Tastaturen mit verschiedene Preise, als Array von Zahlen dargestellt.
+     * 1.
+     * Schreiben Sie eine Methode, die die billigste Tastatur zurückgibt.
+     * z.B. [40 35 70 15 45] => 15
+     * 2.
+     * Schreiben Sie eine Methode, die die teuerste Gegenstand zurückgibt.
+     * z.B. Tastatur = [15 20 10 35], USB = [20, 15, 40 15] => 40
+     * 3.
+     * Schreiben Sie eine Methode, die die teuerste USB Laufwerk, die Markus kaufen kann, zurückgibt.
+     * z.B. Preise = [15 45 20], Budget = 30 => 20
+     * 4.
+     * Finden Sie, anhand des Markus Budget und der Preislisten für die Tastaturen und USB-Laufwerke,
+     * den Geldbetrag Markus ausgeben wird. Wenn er nicht genug für beide hat, geben Sie stattdessen -1 zurück.
+     * Er kauft nur die zwei benötigten Gegenstände.
+     * z.B. b=60, tastaturen = [40 50 60] und usb Laufwerke = [8 12] => 50 + 8 = 58
+     */
+    public int[] tastaturen;
+    public int[] laufwk;
+
+    
 }
+
